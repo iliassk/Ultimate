b049b4d381d19d617d789d0812d45c522c081e50
iliassk <kenfaoui.iliass@gmail.com>
2017-12-23 21:03:35 +0100

    Initial commit

---
+++ AngularJS_Fundamentals/seed/js/todo.controller.js
@@ -0,0 +1,23 @@
+function TodoController() {
+
+  var $ctrl = this;
+  $ctrl.tasksList = [
+    {
+      title: 'Complete Ultimate AngularJS Fundamentals',
+      completed: false
+    },
+    {
+      title: 'Complete Ultimate AngularJS Pro',
+      completed: false
+    },
+    {
+      title: 'Complete Ultimate AngularJS Performance',
+      completed: false
+    }
+  ];
+
+};
+
+angular
+  .module('app')
+  .controller('TodoController', TodoController);
