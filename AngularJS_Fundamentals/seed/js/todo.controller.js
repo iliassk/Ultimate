7863865a9e65439ba3dba4d39c97da5ff463a0c9
iliassk <kenfaoui.iliass@gmail.com>
2017-12-26 13:13:47 +0100

    Lab5 Finished

--- AngularJS_Fundamentals/seed/js/todo.controller.js
+++ AngularJS_Fundamentals/seed/js/todo.controller.js
@@ -1,44 +1,72 @@
-function TodoController() {
+function TodoController(TodoService) {
 
   var $ctrl = this;
   $ctrl.newTodoTitle = '';
-  $ctrl.tasksList = [
-    {
-      title: 'Complete Ultimate AngularJS Fundamentals',
-      completed: false
-    },
-    {
-      title: 'Complete Ultimate AngularJS Pro',
-      completed: true
-    },
-    {
-      title: 'Complete Ultimate AngularJS Performance',
-      completed: false
-    }
-  ];
+  $ctrl.tasksList = [];
+
+  function getTodos() {
+    TodoService
+      .retrieve()
+      .then(function(response) {
+        $ctrl.tasksList = response;
+      });
+  }
 
   function addTodo() {
-    $ctrl.tasksList.unshift({
-      title: $ctrl.newTodoTitle,
-      completed: false
-    });
+    if(!$ctrl.newTodoTitle) {
+      return;
+    }
+    TodoService
+      .create({
+        title: $ctrl.newTodoTitle,
+        completed: false
+      })
+      .then(function(response) {
+        $ctrl.tasksList.unshift(response);
+        $ctrl.newTodoTitle = '';
+      });
+  }
 
-    $ctrl.newTodoTitle = '';
-  };
+  function editTodo(task, index) {
+    if(!task.title) {
+      $ctrl.removeTodo(task, index);
+      return;
+    }
+    TodoService
+      .update(task)
+  }
+
+  function toggleTask(task) {
+    TodoService
+      .update(task)
+      .then(function(response) {
+      })
+      .catch(function(response) {
+        task.completed = !task.completed;
+      });
+  }
 
   function removeTodo(task, index) {
-    $ctrl.tasksList.splice(index, 1);
-  };
+    TodoService
+      .remove(task)
+      .then(function(response) {
+        $ctrl.tasksList.splice(index, 1);
+      });
+  }
 
   function getRemainingTodos() {
     return $ctrl.tasksList.filter(function(todo) {
       return !todo.completed;
     });
-  };
+  }
 
   $ctrl.addTodo = addTodo;
+  $ctrl.editTodo = editTodo;
+  $ctrl.toggleTask = toggleTask;
   $ctrl.removeTodo = removeTodo;
   $ctrl.getRemainingTodos = getRemainingTodos;
+
+  getTodos();
 
 };
 
